e5395e1bee66bf68454a393f2d3ec86256caa5a4
artavazdot <114152164+artavazdot@users.noreply.github.com>
2022-09-24 01:34:06 -0700

    1 file modified, 5 added, 6 deleted

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+import Header from './src/Main';
+import Footer from './src/Navbar';
+
+
+export default function App() {
+  return (
+    <div>
+      <Main />
+      <Navbar/>
+    </div>
+  );
+}
--- src/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer>
-      <small>@ 2022 Toro development. All right reserved</small>
-    </footer>
-  );
-}
